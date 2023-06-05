242b8bdab2dee26b74fb908dd2950808809e3bbc
moh apdi <maxamedcabdifitaax33@gmail.com>
2023-06-05 06:22:45 +0300

    Add project files.

---
+++ Assiment/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Assiment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sizeArray = new System.Windows.Forms.TextBox();
+            this.valueArray = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sizeArray
+            // 
+            this.sizeArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sizeArray.Location = new System.Drawing.Point(167, 62);
+            this.sizeArray.Multiline = true;
+            this.sizeArray.Name = "sizeArray";
+            this.sizeArray.Size = new System.Drawing.Size(171, 53);
+            this.sizeArray.TabIndex = 0;
+            // 
+            // valueArray
+            // 
+            this.valueArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valueArray.Location = new System.Drawing.Point(384, 62);
+            this.valueArray.Multiline = true;
+            this.valueArray.Name = "valueArray";
+            this.valueArray.Size = new System.Drawing.Size(171, 53);
+            this.valueArray.TabIndex = 1;
+            // 
+            // lbl
+            // 
+            this.lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(167, 128);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(388, 296);
+            this.lbl.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(261, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 62);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Arrey";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(182, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Size of array";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(393, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Value of array";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 519);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.valueArray);
+            this.Controls.Add(this.sizeArray);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sizeArray;
+        private System.Windows.Forms.TextBox valueArray;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Assiment/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assiment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int size = int.Parse(sizeArray.Text);
+
+                //int value = Convert.ToInt32(valueArray.Text);
+
+                int[] moh = new int[size];
+
+                //moh[value] = value;
+
+                for (int i = 0; i < size; i++)
+                {
+                    //moh[i] = value + i;
+                    moh[i] = int.Parse(valueArray.Text);
+                }
+                List<int> num = new List<int>();
+                for(int m = 0; m < size; m++)
+                {
+                    for(int j = m + 1; j < size; j++)
+                    {
+                        if(moh[m] != moh[j])
+                        {
+                            num.Add(moh[m]);
+                            num.Add(moh[j]);
+                        }
+                    }
+                }
+
+                lbl.Text = string.Join(" ,", num);
+                //lbl.Text =lbl.Text + " , " + moh.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("error");
+            }
+            
+        }
+    }
+}
